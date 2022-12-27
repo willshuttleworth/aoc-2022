eabf0e31c966da21b072a2724ad360dcbaa69ca6
Will Shuttleworth <93229335+willshuttleworth@users.noreply.github.com>
2022-12-27 10:36:34 -0800

    day5 done

--- day5/day5.py
+++ day5/day5.py
@@ -1,3 +1,49 @@
 f = open("input.txt", 'r')
 lines = f.readlines()
-print(lines[8])
+
+stacks = []
+for i in range(9):
+    stacks.append([])
+
+for i in range(7, -1, -1):
+    line = lines[i].split(' ')
+    j = 0
+    k = 0
+    while k < len(line):
+        if line[k] != '':
+            stacks[j].append(line[k][1])
+            k = k + 1
+        else:
+            k = k + 4
+        j = j + 1
+
+""" part1
+for i in range(10, len(lines)):
+    # 1, 3, 5
+    # count, src, dst
+    line = lines[i].split()
+    count = int(line[1])
+    src = int(line[3])
+    dst = int(line[5])
+    for j in range(count):
+        if(len(stacks[src - 1]) > 0):
+            mov = stacks[src - 1].pop()
+            stacks[dst - 1].append(mov)
+"""
+
+for i in range(10, len(lines)):
+    # 1, 3, 5
+    # count, src, dst
+    line = lines[i].split()
+    count = int(line[1])
+    src = int(line[3])
+    dst = int(line[5])
+    mov = []
+    for j in range(count):
+        if(len(stacks[src - 1]) > 0):
+            mov.insert(0, stacks[src - 1].pop())
+    for j in range(len(mov)):
+        stacks[dst - 1].append(mov[j])
+
+for i in range(9):
+    print(stacks[i][len(stacks[i]) - 1])
