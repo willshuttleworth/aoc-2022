38927989f9bb51ff8d817e629f8a98b688af2478
Will Shuttleworth <93229335+willshuttleworth@users.noreply.github.com>
2022-12-28 22:00:34 -0800

    pretty sure day7 file tree is loaded correctly

---
+++ day7/day7.py
@@ -0,0 +1,59 @@
+class Node:
+    
+    def __init__(self, parent, name, size):
+        self.size = size
+        self.name = name
+        self.parent = parent
+        self.children = []
+    
+    def addchild(self, child):
+        self.children.append(child) 
+
+dirs = []
+# build up tree, keep pointer to all dirs, write a recursive function to find size of each dir in list
+"""
+test = Node(None, 'dir_a', None)
+test.addchild(Node(test, 'file_a', 100))
+print(test.size)
+print(test.children[0].name, test.children[0].size)
+"""
+f = open('input.txt')
+lines = f.readlines()
+root = Node(None, '/', None)
+curr = root
+dirs.append(curr)
+for i in range(1, len(lines)):
+    # possible commands (command line starts with '$'):
+    #   cd `dir`: make curr point to `dir`
+    #   cd .. : make curr point to curr's parent
+    #   ls: read line by line until `$` at first char, adding each entry as a child of curr
+    line = lines[i].split()
+    if line[1] == 'ls':
+        i = i + 1
+        line = lines[i].split()
+        while line[0] != '$':
+            if line[0] == 'dir':
+                child = Node(curr, line[1], None)
+                dirs.append(child)
+                curr.addchild(child)
+            else:
+                child = Node(curr, line[1], line[0])
+                curr.addchild(child)
+            if i < len(lines) - 1:
+                i = i + 1
+                line = lines[i].split()
+            else:
+                break
+
+    elif line[1] == 'cd' and line[2] == '..':
+        curr = curr.parent
+    elif line[1] == 'cd':
+        direc = line[2]
+        for child in curr.children:
+            if child.name == direc:
+                curr = child
+                break
+ 
+
+
+    
